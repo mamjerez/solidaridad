dd7065f49cb24f281b9c552230e5d5fc1a17813e
mamjerez <correomam@gmail.com>
2024-04-12 13:59:41 +0200

    Remove unused JSON data files

--- src/app/art16/components/presupuestos/components/card-info/card-info.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-card-info-home',
-	templateUrl: './card-info.component.html',
-	styleUrls: ['./card-info.component.scss'],
-	standalone: true
-})
-export class CardInfoHomeComponent {}
--- src/app/glosario/glosario.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-glosario',
-	templateUrl: './glosario.component.html',
-	styleUrls: ['./glosario.component.scss'],
-	standalone: true
-})
-export default class GlosarioComponent {}
