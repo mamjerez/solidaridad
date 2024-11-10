01b5905bb5055caa716c2078a052aa7fa048b1fc
mamjerez <correomam@gmail.com>
2024-11-10 18:49:58 +0100

    feat: Add Quienes Somos component with sections for mission, values, and strategic objectives

---
+++ src/app/pages/quienes-somos/quienes-somos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-quienes-somos',
+	standalone: true,
+	imports: [],
+	templateUrl: './quienes-somos.component.html',
+	styleUrl: './quienes-somos.component.scss'
+})
+export default class QuienesSomosComponent {}
