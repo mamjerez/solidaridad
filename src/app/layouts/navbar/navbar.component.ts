af8da7608e41b562373072ea4bff75a39a132d24
mamjerez <correomam@gmail.com>
2024-11-12 18:19:33 +0100

    refactor: Simplify navbar component by removing unused code and updating router links

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,82 +1,18 @@
 import { Location } from '@angular/common';
-import { Component, ElementRef, ViewChild, inject } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
-import NewsFormComponent from '@app/addNewRecod/add-new/add-new.component';
-
-import { environment } from '@environments/environment';
-
-import { ModalService } from '../modal/modal.service';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
 	selector: 'app-navbar',
 	templateUrl: './navbar.component.html',
 	styleUrls: ['./navbar.component.scss'],
 	standalone: true,
-	imports: [RouterOutlet, RouterLink, RouterLinkActive]
+	imports: [RouterOutlet, RouterLink]
 })
 export class NavbarComponent {
-	@ViewChild('modal')
-	modal!: ElementRef<HTMLDivElement>;
-	@ViewChild('overlay') overlay!: ElementRef<HTMLDivElement>;
 	private _location = inject(Location);
-	private _modalService = inject(ModalService);
-	public router = inject(Router);
-	public path: string;
-	public tag: string;
-	public canAddRowSupabase = environment.canAddRowSupabase;
-
-	getTag() {
-		const urlSegments = this.router.url.split('/');
-		this.tag = urlSegments[urlSegments.length - 1];
-	}
-
-	addNew(): void {
-		// this._tagStoreService.getTag();
-		this.getTag();
-		this.router.navigateByUrl('addNew/' + this.tag);
-		// this.useModal();
-	}
-
-	addCom(): void {
-		this.getTag();
-		this.router.navigateByUrl('addCom/' + this.tag);
-	}
-
-	addDoc(): void {
-		this.getTag();
-		this.router.navigateByUrl('addDoc/' + this.tag);
-	}
-
-	addCard(): void {
-		this.getTag();
-		this.router.navigateByUrl('addCard/' + this.tag);
-	}
-
-	navigateTo() {
-		this.router.navigateByUrl('/');
-	}
 
 	volver() {
 		this._location.back();
-	}
-
-	useModal() {
-		this._modalService.open(NewsFormComponent, {
-			animations: {
-				modal: {
-					enter: 'enter-scaling 0.3s ease-out',
-					leave: 'fade-out 0.1s forwards'
-				},
-				overlay: {
-					enter: 'fade-in 1s',
-					leave: 'fade-out 0.3s forwards'
-				}
-			},
-			size: {
-				width: '40rem',
-				minWidth: '850px'
-			}
-		});
 	}
 }
