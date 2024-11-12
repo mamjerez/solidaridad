13d236dc525a14f36475e509599e87b4a0545700
mamjerez <correomam@gmail.com>
2024-11-12 18:24:44 +0100

    refactor: Remove router outlet from app component to streamline layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './layouts/footer/footer.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
-import HomeComponent from './home/home.component';
-// import 'ag-grid-enterprise';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	standalone: true,
-	imports: [NavbarComponent, RouterOutlet, FooterComponent, HomeComponent]
+	imports: [NavbarComponent, FooterComponent]
 })
 export class AppComponent {}
