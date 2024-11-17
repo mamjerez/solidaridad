185a72ca0b26d5904bfa6eed23e4e5a80d5267d4
mamjerez <correomam@gmail.com>
2024-11-17 18:32:27 +0100

    refactor: Simplify historia component by using image arrays for dynamic rendering and clean up unused styles

--- src/app/pages/historia/historia.component.ts
+++ src/app/pages/historia/historia.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 	templateUrl: './historia.component.html',
 	styleUrl: './historia.component.scss'
 })
-export default class HistoriaComponent {}
+export default class HistoriaComponent {
+	memoriaImages = [
+		{ src: '/assets/img/memoria/SebastianPeña.jpg', alt: 'Sebastián Peña' },
+		{ src: '/assets/img/memoria/PedroGarcia.jpg', alt: 'Pedro García' }
+	];
+
+	logrosImages = [
+		{ src: '/assets/img/logros/radioterapia.jpg', alt: 'Radioterapia' },
+		{ src: '/assets/img/logros/1olimpiadas.png', alt: 'Olimpiadas' }
+	];
+
+	recuerdosImages = [
+		{ src: '/assets/img/recuerdos/ReyesMagos.jpg', alt: 'Reyes Magos' },
+		{ src: '/assets/img/recuerdos/PlataformaA2003.jpg', alt: 'Plataforma A2003' },
+		{ src: '/assets/img/recuerdos/ADIFI.jpg', alt: 'ADIFI' },
+		{ src: '/assets/img/recuerdos/SebastianPedro.jpg', alt: 'Sebastian y Pedro' },
+		{ src: '/assets/img/recuerdos/FiestaVendimia.jpg', alt: 'Fiesta Vendimia' },
+		{ src: '/assets/img/recuerdos/ReyesMagos.jpg', alt: 'Reyes Magos' },
+		{ src: '/assets/img/recuerdos/PlataformaA2003.jpg', alt: 'Plataforma A2003' },
+		{ src: '/assets/img/recuerdos/ADIFI.jpg', alt: 'ADIFI' },
+		{ src: '/assets/img/recuerdos/SebastianPedro.jpg', alt: 'Sebastian y Pedro' },
+		{ src: '/assets/img/recuerdos/FiestaVendimia.jpg', alt: 'Fiesta Vendimia' }
+	];
+}
