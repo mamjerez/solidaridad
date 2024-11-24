b1097bcc0a3ab140de71a0f255f39601816cab23
mamjerez <correomam@gmail.com>
2024-11-24 16:33:10 +0100

    refactor: Simplify "Quiénes Somos" page structure and styles; remove unused sections and clean up SCSS

--- src/app/pages/quienes-somos/quienes-somos.component.ts
+++ src/app/pages/quienes-somos/quienes-somos.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-quienes-somos',
 	standalone: true,
-	imports: [],
 	templateUrl: './quienes-somos.component.html',
 	styleUrl: './quienes-somos.component.scss'
 })
