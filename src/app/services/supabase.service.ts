79e7c72af7410520d7401483a1b527d100e01e28
mamjerez <correomam@gmail.com>
2024-11-24 16:54:59 +0100

    refactor: Remove EnsureTitleService and ScreenSizeService; clean up SupabaseService by removing unused methods

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,10 +1,7 @@
-// https://supabase.com/docs/guides/getting-started/tutorials/with-angular
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-
-// import fileData from 'D:/create-licitacion/jpgFiles.json';
 
 @Injectable({
 	providedIn: 'root'
@@ -25,21 +22,10 @@ export class SupabaseService {
 
 		const todayStr = today.toISOString().split('T')[0];
 		const yesterdayStr = yesterday.toISOString().split('T')[0];
-
 		const { data, error } = await this._supabase.from('news').select('*').in('date', [todayStr, yesterdayStr]);
 
 		if (error) throw error;
-		console.log('data', data);
-
 		return data || [];
-	}
-
-	async fetchData1(): Promise<any> {
-		const { data, error } = await this._supabase.rpc('obtener_detalle_entidad_organizativa', {
-			p_id_entidad_organizativa: 10
-		});
-		if (error) throw error;
-		console.log(data);
 	}
 
 	// TODO: - Add types
@@ -56,110 +42,6 @@ export class SupabaseService {
 		return data;
 	}
 
-	async fetchDataById(tableName: string, id: number): Promise<any> {
-		const { data, error } = await this._supabase.from(tableName).select('*').eq('id', id);
-		if (error) throw error;
-		// console.log('data', data);
-
-		return data;
-	}
-
-	async fetchDataByIdeo(tableName: string, id: number): Promise<any> {
-		const { data, error } = await this._supabase.from(tableName).select('*').eq('id_eo', id);
-		if (error) throw error;
-		// console.log('data', data);
-
-		return data;
-	}
-
-	async fetchDataByIduo(tableName: string, id: number): Promise<any> {
-		const { data, error } = await this._supabase.from(tableName).select('*').eq('id_uo', id);
-		if (error) throw error;
-		// console.log('data', data);
-
-		return data;
-	}
-
-	async fetchDataByIdpuestouo(tableName: string, id: number): Promise<any> {
-		const { data, error } = await this._supabase.from(tableName).select('*').eq('id_uo', id);
-		if (error) throw error;
-		// console.log('data', data);
-
-		return data;
-	}
-
-	async fetchDataByIdPuesto(tableName: string, id: number): Promise<any> {
-		const { data, error } = await this._supabase.from(tableName).select('*').eq('id_puesto', id);
-		if (error) throw error;
-		return data;
-	}
-
-	async fetchDataByIdEmpleado(tableName: string, id: number): Promise<any> {
-		const { data, error } = await this._supabase.from(tableName).select('*').eq('id_empleado', id);
-		if (error) throw error;
-		return data;
-	}
-
-	async fetchDataByIdString(tableName: string, id: string) {
-		// console.log('id', id);
-
-		const { data, error } = await this._supabase.from(tableName).select('*').eq('id', +id);
-		if (+id === 284) {
-			console.log('data', data);
-		}
-
-		if (error) throw error;
-		return data;
-	}
-
-	async fetchDataByTag(tableName: string, tag: string): Promise<any> {
-		const { data, error } = await this._supabase.from(tableName).select('*').eq('tag', tag);
-
-		if (error) throw error;
-		return data;
-	}
-
-	async fetchDataByCodigo(tableName: string, codigo: string): Promise<any> {
-		console.log('codigo', codigo);
-
-		const { data, error } = await this._supabase.from(tableName).select('*').eq('codigo', codigo);
-
-		if (error) throw error;
-		return data;
-	}
-
-	// async fetchDataByLevel(level: string, tag?: string): Promise<any> {
-	// 	const { data, error } = await this._supabase.from('tag_title').select('*').eq('level', level);
-	// 	const dataFiltered = tag ? data.filter((item) => item['levelUp'] === tag) : data;
-
-	// 	if (error) throw error;
-	// 	return dataFiltered.sort((a, b) => a.order - b.order);
-	// }
-
-	async fetchDataByLevel(level: string, tag?: string): Promise<any> {
-		const { data, error } = await this._supabase
-			.from('solidaridad_menu_cards')
-			.select('id,tag,title,level,has_imagen,is_last_level,is_visible,orden,level_up,url_externa')
-			.eq('level', level);
-		const dataFiltered = data
-			.filter((item) => item.is_visible === true)
-			.filter((item) => (tag ? item['level_up'] === tag : true));
-
-		if (error) throw error;
-
-		return dataFiltered.sort((a, b) => a.orden - b.orden);
-	}
-
-	async fetchDataByFinanciacion(financiacion: string): Promise<any> {
-		const { data, error } = await this._supabase
-			.from('licitaciones-cards')
-			.select('*')
-			.eq('financiacion', financiacion);
-
-		if (error) throw error;
-		return data.sort((a, b) => a.order - b.order);
-	}
-
 	async fetchDataByTagOrder(tableName: string, tag: string, order: boolean): Promise<any> {
 		const { data, error } = await this._supabase
 			.from(tableName)
@@ -167,13 +49,6 @@ export class SupabaseService {
 			.eq('tag', tag)
 			.order('date', { ascending: order });
 		if (error) throw error;
-		return data;
-	}
-
-	async fetchDataFromView(view: string, id: number | string): Promise<any> {
-		const { data, error } = await this._supabase.from(view).select('*').eq('id', id);
-		if (error) throw error;
-		console.log(JSON.stringify(data));
 		return data;
 	}
 
@@ -191,57 +66,5 @@ export class SupabaseService {
 
 		if (error) throw error;
 		return data;
-	}
-
-	// uploadFile = async (bucketName, filePath, fileData?, options?) => {
-	// 	console.log('uploadFile');
-
-	// 	try {
-	// 		// Obtener la imagen como blob
-	// 		const response = await fetch(filePath);
-	// 		if (!response.ok) throw new Error('Network response was not ok');
-	// 		const imageBlob = await response.blob();
-
-	// 		// Cargar el blob a Supabase
-	// 		const { data, error } = await this._supabase.storage.from(bucketName).upload(filePath, imageBlob, options);
-
-	// 		if (error) throw error;
-
-	// 		console.log(`File uploaded to ${filePath}`, data);
-	// 	} catch (error) {
-	// 		console.error('Error:', error);
-	// 	}
-	// };
-
-	async uploadFile(file) {
-		const filePath = `${file.name}`; // Asegúrate de cambiar 'nombre_del_directorio' al directorio deseado en Supabase Storage
-
-		try {
-			const { error, data } = await this._supabase.storage.from('imgEmpleados').upload(filePath, file);
-
-			if (error) {
-				throw error;
-			}
-
-			// console.log('Archivo subido con éxito:', data);
-		} catch (error) {
-			console.error('Error en la subida del archivo:', error);
-		}
-	}
-
-	async uploadFileFromJSON(fileData) {
-		const filePath = `${fileData.name}`; // Asegúrate de cambiar 'nombre_del_directorio' al directorio deseado en Supabase Storage
-
-		try {
-			const { error, data } = await this._supabase.storage.from('imgParaClasificar').upload(filePath, filePath);
-
-			if (error) {
-				throw error;
-			}
-
-			// console.log('Archivo subido con éxito:', data);
-		} catch (error) {
-			console.error('Error en la subida del archivo:', error);
-		}
 	}
 }
