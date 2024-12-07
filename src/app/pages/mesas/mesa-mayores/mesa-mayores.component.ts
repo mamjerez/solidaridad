1f4401b2680e0d1564c5f1a42376d7187b54c6a1
mamjerez <correomam@gmail.com>
2024-12-07 19:12:01 +0100

    feat: add 'Mesa Mayores' component with comment functionality and routing

---
+++ src/app/pages/mesas/mesa-mayores/mesa-mayores.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { InformacionesComponent } from '@app/commons/components/informaciones/informaciones.component';
+
+@Component({
+	selector: 'app-mayores',
+	standalone: true,
+	imports: [InformacionesComponent],
+	templateUrl: './mesa-mayores.component.html',
+	styleUrl: './mesa-mayores.component.scss'
+})
+export default class MesaMayoresComponent {
+	private readonly router = inject(Router);
+
+	public tag = 'mesaTurismo';
+
+	addCom(): void {
+		this.router.navigateByUrl('addCom/' + this.tag);
+	}
+}
