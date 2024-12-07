2e78069f662ecb3ffaf5ea7d07e784b27746d7e6
mamjerez <correomam@gmail.com>
2024-12-07 09:56:16 +0100

    feat: add Misión, Visión, and Valores components with corresponding styles and navigation

---
+++ src/app/pages/mision/mision.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-mision',
+	standalone: true,
+	imports: [],
+	templateUrl: './mision.component.html',
+	styleUrl: './mision.component.scss'
+})
+export default class MisionComponent {}
---
+++ src/app/pages/valores/valores.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-valores',
+	standalone: true,
+	imports: [],
+	templateUrl: './valores.component.html',
+	styleUrl: './valores.component.scss'
+})
+export default class ValoresComponent {}
