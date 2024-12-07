4fe907bec76840315dca16d55c6c5571fe9efedd
mamjerez <correomam@gmail.com>
2024-12-07 19:12:17 +0100

    feat: add 'Mesa Seguridad' component with comment functionality and routing

---
+++ src/app/pages/mesas/mesa-seguridad/mesa-seguridad.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { InformacionesComponent } from '@app/commons/components/informaciones/informaciones.component';
+
+@Component({
+	selector: 'app-mesa-seguridad',
+	standalone: true,
+	imports: [InformacionesComponent],
+	templateUrl: './mesa-seguridad.component.html',
+	styleUrl: './mesa-seguridad.component.scss'
+})
+export default class MesaSeguridadComponent {
+	private readonly router = inject(Router);
+
+	public tag = 'mesaSeguridad';
+
+	addCom(): void {
+		this.router.navigateByUrl('addCom/' + this.tag);
+	}
+}
