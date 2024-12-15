fcc4528f32db1ef3d3c9d168afe2565b55129083
mamjerez <correomam@gmail.com>
2024-12-15 08:55:23 +0100

    feat: conditionally display footer based on route navigation in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './layouts/footer/footer.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
@@ -12,4 +12,16 @@ import { CookieConsentComponent } from './commons/components/cookie-consent.comp
 	standalone: true,
 	imports: [NavbarComponent, FooterComponent, RouterOutlet, CookieConsentComponent]
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+	private _router = inject(Router);
+	public showFooter = true;
+
+	ngOnInit() {
+		this._router.events.subscribe((event) => {
+			if (event instanceof NavigationEnd) {
+				const rutasSinFooter = ['/laPlata', '/ruta2']; // Rutas donde se oculta el footer
+				this.showFooter = !rutasSinFooter.includes(event.urlAfterRedirects);
+			}
+		});
+	}
+}
