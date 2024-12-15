8c2319dcc2d12dcc13f81c8f8460918fa7feb7c8
mamjerez <correomam@gmail.com>
2024-12-15 08:18:21 +0100

    feat: enhance LaPlataComponent with new sections for activities and history; integrate noticias components for neighborhood and association news

--- src/app/pages/asociaciones-federadas/la-plata/la-plata.component.ts
+++ src/app/pages/asociaciones-federadas/la-plata/la-plata.component.ts
@@ -1,24 +1,34 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CardMenuComponent } from '@app/commons/components/card-menu/card-menu.component';
+import { NoticiasAsociacionComponent } from '@app/commons/components/noticias-asociacion/noticias-asociacion.component';
+import { NoticiasBarrioComponent } from '@app/commons/components/noticias-barrio/noticias-barrio.component';
 import { ICard } from '@interfaces/card.interface';
+import { INew } from '@interfaces/new.interface';
 import { SupabaseService } from '@services/supabase.service';
 
 @Component({
 	selector: 'app-la-plata',
 	standalone: true,
-	imports: [CardMenuComponent],
+	imports: [CardMenuComponent, NoticiasBarrioComponent, NoticiasAsociacionComponent],
 	templateUrl: './la-plata.component.html',
 	styleUrl: './la-plata.component.scss'
 })
 export default class LaPlataComponent implements OnInit {
 	private readonly _supabaseService = inject(SupabaseService);
 	private readonly _router = inject(Router);
-	private _pathImage = 'https://cswdadlxiubwdzvqzywc.supabase.co/storage/v1/object/public/laPlata/';
+	private readonly _pathImage = 'https://cswdadlxiubwdzvqzywc.supabase.co/storage/v1/object/public/laPlata/';
 	public cards: ICard[] = [];
+	public cardsActividades: ICard[] = [];
+	public cardsHistoria: ICard[] = [];
+	public newsBarrio: INew[] = [];
+	public newsAsociacion: INew[] = [];
 
 	ngOnInit() {
 		this.createCardMenu();
+		this.createCardActividad();
+		this.createCardHistoria();
+		this.fetchNews();
 	}
 
 	async createCardMenu() {
@@ -32,5 +42,36 @@ export default class LaPlataComponent implements OnInit {
 		} catch (error) {
 			console.error('Error fetching data:', error);
 		}
+	}
+
+	async createCardActividad() {
+		try {
+			const data = await this._supabaseService.fetchDataHomeAVV('laplata_actividades');
+			this.cardsActividades = data.map((card) => ({
+				...card,
+				rutaImagen: `${this._pathImage}${card.tag}.jpg`,
+				funcion: () => this._router.navigateByUrl(card.tag, { state: { avv: 'laPlata' } })
+			}));
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
+	}
+
+	async createCardHistoria() {
+		try {
+			const data = await this._supabaseService.fetchDataHomeAVV('laplata_historia_cards');
+			this.cardsHistoria = data.map((card) => ({
+				...card,
+				rutaImagen: `${this._pathImage}${card.tag}.jpg`,
+				funcion: () => this._router.navigateByUrl(card.tag, { state: { avv: 'laPlata' } })
+			}));
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
+	}
+
+	async fetchNews() {
+		this.newsBarrio = await this._supabaseService.fetchDataByTagOrder('news', 'barriadaLaPlata', false);
+		this.newsAsociacion = await this._supabaseService.fetchDataByTagOrder('solidaridad_news', 'laPlata', false);
 	}
 }
