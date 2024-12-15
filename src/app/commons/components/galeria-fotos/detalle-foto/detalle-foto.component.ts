a0afa32ec7ba78005faab47d514456fcc9d02232
mamjerez <correomam@gmail.com>
2024-12-15 16:39:44 +0100

    feat: enhance DetalleFotoComponent with buttons and comments display, and improve layout styling

--- src/app/commons/components/galeria-fotos/detalle-foto/detalle-foto.component.ts
+++ src/app/commons/components/galeria-fotos/detalle-foto/detalle-foto.component.ts
@@ -1,11 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SupabaseService } from '@services/supabase.service';
+import { BotonesAddComponent } from '../../botones-add/botones-add.component';
+import ComentariosComponent from '../../comentarios/comentarios.component';
 
 @Component({
 	selector: 'app-detalle-foto',
 	standalone: true,
-	imports: [],
+	imports: [ComentariosComponent, BotonesAddComponent],
+
 	templateUrl: './detalle-foto.component.html',
 	styleUrl: './detalle-foto.component.scss'
 })
@@ -13,14 +16,22 @@ export default class DetalleFotoComponent implements OnInit {
 	private readonly _route = inject(ActivatedRoute);
 	private readonly _supabaseService = inject(SupabaseService);
 	public foto: { url: string; descripcion: string; id: string; fecha: string } | null = null;
+	public tag = '';
+	public coms: [] = [];
 
 	async ngOnInit() {
 		const id = this._route.snapshot.paramMap.get('id');
 		if (id) {
 			const data = await this._supabaseService.fetchDataById('laPlata_fotos', id);
 			this.foto = data[0];
+			this.tag = this.foto.id;
 		} else {
 			console.error('ID no encontrado en la ruta');
 		}
+		this.cargarComentarios();
+	}
+
+	async cargarComentarios() {
+		this.coms = await this._supabaseService.fetchDataByTag('solidaridad_comentarios', this.tag);
 	}
 }
