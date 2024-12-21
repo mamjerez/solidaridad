6176b7503e66ca469199e0e9a8fedb15e51c10ae
mamjerez <correomam@gmail.com>
2024-12-21 13:29:42 +0100

    refactor: update noticias-asociacion component to use function calls for newsAsociacion and improve input handling

--- src/app/commons/components/noticias-asociacion/noticias-asociacion.component.ts
+++ src/app/commons/components/noticias-asociacion/noticias-asociacion.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
 	selector: 'app-noticias-asociacion',
-	standalone: true,
 	imports: [CommonModule],
 	templateUrl: './noticias-asociacion.component.html',
 	styleUrl: './noticias-asociacion.component.scss'
 })
 export class NoticiasAsociacionComponent {
-	@Input() newsAsociacion: any[];
+	readonly newsAsociacion = input<any[]>(undefined);
 }
