a01c5d1a7afbbf6a714f29496d1e5831a4e35d84
mamjerez <correomam@gmail.com>
2024-12-21 13:28:49 +0100

    refactor: update noticias-barrio component to use function calls for newsBarrio and improve input handling

--- src/app/commons/components/noticias-barrio/noticias-barrio.component.ts
+++ src/app/commons/components/noticias-barrio/noticias-barrio.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
 	selector: 'app-noticias-barrio',
-	standalone: true,
 	imports: [CommonModule],
 	templateUrl: './noticias-barrio.component.html',
 	styleUrl: './noticias-barrio.component.scss'
 })
 export class NoticiasBarrioComponent {
-	@Input() newsBarrio: any[];
+	readonly newsBarrio = input<any[]>(undefined);
 }
