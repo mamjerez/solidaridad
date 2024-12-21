0fb46abef18660359f07751e58a2f7b991331c5d
mamjerez <correomam@gmail.com>
2024-12-21 13:07:19 +0100

    refactor: remove proyectos-card component and related data and model files to simplify the codebase

--- src/app/pages/proyectos/components/proyectos.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Project {
-	id: number;
-	title: string;
-	description: string;
-	image: string;
-	status: 'Presentado' | 'En Progreso' | 'Completado' | 'Planificado';
-	progress: number;
-}
--- src/app/pages/proyectos/proyectos.component.ts
+++ src/app/pages/proyectos/proyectos.component.ts
@@ -1,25 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { PROJECTS } from './components/proyectos.data';
 import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-proyectos',
-	standalone: true,
 	templateUrl: './proyectos.component.html',
 	styleUrl: './proyectos.component.scss'
 })
 export default class ProyectosCardComponent {
 	private readonly _router = inject(Router);
-	projects = PROJECTS;
-	filteredProjects = PROJECTS;
-	showProjectForm = false;
-
-	onFilterChange(status: string) {
-		this.filteredProjects =
-			status === 'Todos' ? this.projects : this.projects.filter((project) => project.status === status);
-	}
-
-	navigateTo(path: string) {
-		this._router.navigate([path]);
-	}
 }
