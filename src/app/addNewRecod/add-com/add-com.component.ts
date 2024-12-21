b496ef203b41fff54976007c7ff7fca0ac8e532b
mamjerez <correomam@gmail.com>
2024-12-21 08:43:15 +0100

    refactor: enhance tag display in add-com component and clean up unused code

--- src/app/addNewRecod/add-com/add-com.component.ts
+++ src/app/addNewRecod/add-com/add-com.component.ts
@@ -1,40 +1,20 @@
 import { Location } from '@angular/common';
-import { Component, Input, OnInit, inject } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit, inject, input } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { SupabaseService } from '@services/supabase.service';
-import { ModalService } from '@app/layouts/modal/modal.service';
-
-interface IAsociaciones {
-	id: number;
-	created_at: string; // Asumiendo que siempre recibes la fecha como cadena
-	nombre: string;
-	rma: number;
-	presidente: string;
-	sede: string;
-	barrio: string | null; // 'null' explicitado como posible valor
-	telefono: string | null;
-	contacto: string;
-	email: string | null;
-	email1: string | null;
-	distrito: string;
-	tag: string;
-}
 
 @Component({
 	selector: 'app-coms-form',
-	standalone: true,
-	imports: [FormsModule, ReactiveFormsModule],
+	imports: [ReactiveFormsModule],
 	templateUrl: './add-com.component.html'
 })
 export default class AddComComponent implements OnInit {
-	@Input() tag: string;
-	comForm: FormGroup;
+	readonly tag = input.required<string>();
+	public comForm: FormGroup;
 	private _formBuilder = inject(FormBuilder);
 	private _supabaseService = inject(SupabaseService);
 	private _location = inject(Location);
-	private _modalService = inject(ModalService);
-	public data: IAsociaciones = null;
 
 	async ngOnInit(): Promise<void> {
 		this.comForm = this._formBuilder.group({
@@ -43,32 +23,17 @@ export default class AddComComponent implements OnInit {
 			text: ['', Validators.required],
 			confidencial: [false, Validators.required]
 		});
-
-		console.log('Tag:', this.tag);
-
-		// try {
-		// 	this.data = await this._supabaseService.fetchDataById('asociaciones', this.tag);
-		// 	console.log('data:', this.data);
-		// } catch (error) {
-		// 	console.error('Error fetching data:', error);
-		// }
 	}
 
 	async guardar(): Promise<void> {
 		if (this.comForm?.valid) {
 			const formData = {
 				...this.comForm.value,
-				// tag: this.data[0].tag
-				tag: this.tag
+				tag: this.tag()
 			};
 
-			try {
-				await this._supabaseService.insertRow('solidaridad_comentarios', formData);
-				// this._modalService.close(); // NO FUNCIONA
-				this._location.back();
-			} catch (error) {
-				console.error('Error al insertar datos:', error);
-			}
+			await this._supabaseService.insertRow('solidaridad_comentarios', formData);
+			this._location.back();
 		}
 	}
 }
