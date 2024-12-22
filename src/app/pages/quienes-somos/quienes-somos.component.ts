700b7d83c4f55b30d9188e8310d45f6a3a3cf607
mamjerez <correomam@gmail.com>
2024-12-22 09:42:22 +0100

    refactor: update quienes-somos component layout by replacing card menu with header and image

--- src/app/pages/quienes-somos/quienes-somos.component.ts
+++ src/app/pages/quienes-somos/quienes-somos.component.ts
@@ -1,18 +1,8 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { CardMenuComponent } from '@app/commons/components/card-menu/card-menu.component';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-quienes-somos',
-
-	imports: [CardMenuComponent],
 	templateUrl: './quienes-somos.component.html',
 	styleUrl: './quienes-somos.component.scss'
 })
-export default class QuienesSomosComponent {
-	private readonly _router = inject(Router);
-
-	navigateTo(path: string) {
-		this._router.navigate([path]);
-	}
-}
+export default class QuienesSomosComponent {}
