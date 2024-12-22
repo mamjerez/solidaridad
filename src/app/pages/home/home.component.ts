ac2140681e7f895230d8781286f9328ead382809
mamjerez <correomam@gmail.com>
2024-12-22 09:00:55 +0100

    refactor: update image class and paths in home component for consistency

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,20 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { CardMenuComponent } from '@app/commons/components/card-menu/card-menu.component';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
-
 	imports: [CardMenuComponent]
 })
 export default class HomeComponent implements OnInit {
 	private readonly _router = inject(Router);
 	public images = [
-		'/assets/img/inicio/juntaDirectiva.jpg',
-		'/assets/img/inicio/2024Pelayo.jpg',
-		'/assets/img/inicio/2022Mamen.jpg'
+		'/assets/img/home/juntaDirectiva.jpg',
+		'/assets/img/home/2024Pelayo.jpg',
+		'/assets/img/home/2022Mamen.jpg'
 	];
 	public currentImageIndex = 0;
 
