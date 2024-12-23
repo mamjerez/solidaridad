4c5796a62f29ed1266f8518fd0d31960336db09c
mamjerez <correomam@gmail.com>
2024-12-23 05:10:29 +0100

    refactor: remove unnecessary whitespace in secretarias component for cleaner code

--- src/app/pages/secretarias/secretarias.component.ts
+++ src/app/pages/secretarias/secretarias.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { CardMenuComponent } from '@app/commons/components/card-menu/card-menu.component';
 
 @Component({
 	selector: 'app-secretarias',
-
 	imports: [CardMenuComponent],
 	templateUrl: './secretarias.component.html',
 	styleUrl: './secretarias.component.scss'
