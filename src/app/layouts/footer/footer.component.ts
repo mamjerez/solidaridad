1e6f4941fb07883fe9dde1e16d6a247d38427bb0
mamjerez <correomam@gmail.com>
2024-12-23 04:47:59 +0100

    refactor: remove unused footer markup and version reference for cleanup

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import packageJson from '../../../../package.json';
 
 @Component({
 	selector: 'app-footer',
 	templateUrl: './footer.component.html',
 	styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-	version = packageJson.version;
-}
+export class FooterComponent {}
