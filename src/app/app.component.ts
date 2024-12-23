34714bf28130ac30e2d3395e70a8adae5f68a371
mamjerez <correomam@gmail.com>
2024-12-23 04:44:24 +0100

    refactor: rename variable for clarity in footer visibility logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,23 @@ import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './layouts/footer/footer.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
-
 import { CookieConsentComponent } from './commons/components/cookie-consent.component';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-
 	imports: [NavbarComponent, FooterComponent, RouterOutlet, CookieConsentComponent]
 })
 export class AppComponent implements OnInit {
 	private _router = inject(Router);
-	private cadenasOcultarFooter = ['laPlata', 'LaPlata', 'foto', 'resumenPrensa'];
-
+	private _cadenasOcultarFooter = ['laPlata', 'LaPlata', 'foto', 'resumenPrensa'];
 	public showFooter = true;
 
 	ngOnInit() {
 		this._router.events.subscribe((event) => {
 			if (event instanceof NavigationEnd) {
 				const urlActual = event.urlAfterRedirects;
-				this.showFooter = !this.cadenasOcultarFooter.some((cadena) => urlActual.includes(cadena));
+				this.showFooter = !this._cadenasOcultarFooter.some((cadena) => urlActual.includes(cadena));
 			}
 		});
 	}
