5817127eee912313eb59728f1ab0fff316606ab8
mamjerez <correomam@gmail.com>
2024-12-23 05:11:27 +0100

    refactor: remove unnecessary whitespace in fecha20241210 and secretarias-reuniones components for cleaner code

--- src/app/pages/secretarias-reuniones/reuniones/fecha20241210/fecha20241210.component.ts
+++ src/app/pages/secretarias-reuniones/reuniones/fecha20241210/fecha20241210.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { InformacionesComponent } from '@app/commons/components/informaciones/informaciones.component';
 
 @Component({
 	selector: 'app-fecha20241210',
-
 	imports: [InformacionesComponent],
 	templateUrl: './fecha20241210.component.html',
 	styleUrl: './fecha20241210.component.scss'
--- src/app/pages/secretarias-reuniones/secretarias-reuniones.component.ts
+++ src/app/pages/secretarias-reuniones/secretarias-reuniones.component.ts
@@ -1,17 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { CardMenuComponent } from '@app/commons/components/card-menu/card-menu.component';
 
 @Component({
 	selector: 'app-secretarias-reuniones',
-
 	imports: [CardMenuComponent],
 	templateUrl: './secretarias-reuniones.component.html',
 	styleUrl: './secretarias-reuniones.component.scss'
 })
 export default class SecretariasReunionesComponent {
 	private readonly router = inject(Router);
-
 	public tag = '20241210';
 	navigateTo(page: string): void {
 		this.router.navigate([page]);
