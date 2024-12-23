28c91bee1e084aa95bb078775d0c7bed357097b9
mamjerez <correomam@gmail.com>
2024-12-23 05:02:38 +0100

    refactor: remove unnecessary whitespace in historia component for cleaner code

--- src/app/pages/historia/historia.component.ts
+++ src/app/pages/historia/historia.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-historia',
-
 	imports: [],
 	templateUrl: './historia.component.html',
 	styleUrl: './historia.component.scss'
