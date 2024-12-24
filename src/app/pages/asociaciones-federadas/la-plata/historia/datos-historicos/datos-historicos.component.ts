fd5bd3ea357fcc6d19754abc6b5e72ee2325321f
mamjerez <correomam@gmail.com>
2024-12-24 06:13:11 +0100

    feat: add DatosHistoricosComponent with HTML template and TypeScript logic

---
+++ src/app/pages/asociaciones-federadas/la-plata/historia/datos-historicos/datos-historicos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datos-historicos',
+  imports: [],
+  templateUrl: './datos-historicos.component.html',
+  styleUrl: './datos-historicos.component.scss'
+})
+export class DatosHistoricosComponent {
+
+}
