eeaed1a7c352e70914c88773f0bc959170d2e2b4
mamjerez <correomam@gmail.com>
2024-12-24 06:13:22 +0100

    feat: add FamiliasHistoricasComponent with HTML template and TypeScript logic

---
+++ src/app/pages/asociaciones-federadas/la-plata/historia/familias-historicas/familias-historicas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-familias-historicas',
+  imports: [],
+  templateUrl: './familias-historicas.component.html',
+  styleUrl: './familias-historicas.component.scss'
+})
+export class FamiliasHistoricasComponent {
+
+}
