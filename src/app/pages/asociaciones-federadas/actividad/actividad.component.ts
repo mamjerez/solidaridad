66330d3890841b65fcb82d4f88ce4fb77b950c06
mamjerez <correomam@gmail.com>
2024-12-24 16:14:06 +0100

    feat: add new components for activity and historical data pages; implement admin functionality for adding photos

--- src/app/pages/asociaciones-federadas/actividad/actividad.component.ts
+++ src/app/pages/asociaciones-federadas/actividad/actividad.component.ts
@@ -1,9 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { BotonesAddComponent } from '@app/commons/components/botones-add/botones-add.component';
+import { GaleriaFotosComponent } from '@app/commons/components/galeria-fotos/galeria-fotos.component';
+import { InformacionesComponent } from '@app/commons/components/informaciones/informaciones.component';
+
 @Component({
 	selector: 'app-actividad',
-
+	imports: [GaleriaFotosComponent, InformacionesComponent, BotonesAddComponent],
 	templateUrl: './actividad.component.html',
 	styleUrl: './actividad.component.scss'
 })
--- src/app/pages/asociaciones-federadas/historia/familias-historicas/familias-historicas.component.ts
+++ src/app/pages/asociaciones-federadas/historia/familias-historicas/familias-historicas.component.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+
+import { GaleriaFotosComponent } from '@app/commons/components/galeria-fotos/galeria-fotos.component';
+
+import { IsAdminService } from '@services/isAdmin.service';
 
 @Component({
 	selector: 'app-familias-historicas',
-	imports: [],
+	imports: [GaleriaFotosComponent],
+
 	templateUrl: './familias-historicas.component.html',
 	styleUrl: './familias-historicas.component.scss'
 })
-export default class FamiliasHistoricasComponent {}
+export default class FamiliasHistoricasComponent implements OnInit {
+	private readonly _location = inject(Location);
+	private readonly _router = inject(Router);
+	private readonly _isAdminService = inject(IsAdminService);
+	public isAdmin: boolean;
+	public tag = '';
+	public data: any;
+
+	constructor() {
+		// Hay que hacerlo en el constructor de lo contrario no funciona
+		const navigation = this._router.getCurrentNavigation();
+		this.data = navigation?.extras.state?.['data'];
+		this.tag = this.data.tag + this.data.asociacion;
+	}
+
+	ngOnInit(): void {
+		this._isAdminService.isAdmin$.subscribe((value) => {
+			this.isAdmin = value;
+		});
+	}
+
+	volver(): void {
+		this._location.back();
+	}
+
+	addFoto(): void {
+		this._router.navigateByUrl('addFoto/' + this.tag);
+	}
+}
