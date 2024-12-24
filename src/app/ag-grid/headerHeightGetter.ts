6ac95317312ffb0abe4390972a103a0ac86ff816
mamjerez <correomam@gmail.com>
2024-12-24 10:23:34 +0100

    refactor: remove unused ag-grid header component and related styles

--- src/app/ag-grid/headerHeightGetter.ts
+++
@@ -1,8 +0,0 @@
-export function headerHeightGetter() {
-	const columnHeaderTexts = document.querySelectorAll('.ag-header-cell-text');
-	const columnHeaderTextsArray: Element[] = [];
-	columnHeaderTexts.forEach((node) => columnHeaderTextsArray.push(node));
-	const clientHeights = columnHeaderTextsArray.map((headerText) => headerText.clientHeight);
-	const tallestHeaderTextHeight = Math.max(...clientHeights);
-	return tallestHeaderTextHeight;
-}
