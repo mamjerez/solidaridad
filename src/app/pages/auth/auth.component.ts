997a462e6eadb4fd7ab19b5b65715b2e36c2f3dd
mamjerez <correomam@gmail.com>
2025-01-05 13:22:42 +0100

    refactor: remove unused LOCALE_ID import in AuthComponent

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, LOCALE_ID } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
