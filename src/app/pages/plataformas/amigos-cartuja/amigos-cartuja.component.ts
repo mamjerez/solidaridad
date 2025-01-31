7638ca1a00fc7f8e2726fd501df7fc91870f13ed
mamjerez <correomam@gmail.com>
2025-01-31 18:02:55 +0100

    feat: replace button with app-botones-add component in amigos-cartuja and asta-regia components

--- src/app/pages/plataformas/amigos-cartuja/amigos-cartuja.component.ts
+++ src/app/pages/plataformas/amigos-cartuja/amigos-cartuja.component.ts
@@ -1,19 +1,19 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
+import { BotonesAddComponent } from '@app/commons/components/botones-add/botones-add.component';
 import { InformacionesComponent } from '@app/commons/components/informaciones/informaciones.component';
 
 @Component({
 	selector: 'app-amigos-cartuja',
-	imports: [InformacionesComponent],
+	imports: [InformacionesComponent, BotonesAddComponent],
 	templateUrl: './amigos-cartuja.component.html',
 	styleUrl: './amigos-cartuja.component.scss'
 })
 export default class AmigosCartujaComponent {
-	private readonly router = inject(Router);
+	// private readonly router = inject(Router);
 	public tag = 'cartuja';
 
-	addCom(): void {
-		this.router.navigateByUrl('addCom/' + this.tag);
-	}
+	// addCom(): void {
+	// 	this.router.navigateByUrl('addCom/' + this.tag);
+	// }
 }
--- src/app/pages/plataformas/asta-regia/asta-regia.component.ts
+++ src/app/pages/plataformas/asta-regia/asta-regia.component.ts
@@ -1,31 +1,31 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 import { InformacionesComponent } from '@app/commons/components/informaciones/informaciones.component';
+import { BotonesAddComponent } from '@app/commons/components/botones-add/botones-add.component';
 
-import { GetNewsComsDocs } from '@services/getNewsComsDocs.service';
+// import { GetNewsComsDocs } from '@services/getNewsComsDocs.service';
 
-import { ICom } from '@interfaces/com.interface';
-import { IDoc } from '@interfaces/doc.interface';
-import { INew } from '@interfaces/new.interface';
-import { IGestion } from '@interfaces/gestion.interface';
+// import { ICom } from '@interfaces/com.interface';
+// import { IDoc } from '@interfaces/doc.interface';
+// import { INew } from '@interfaces/new.interface';
+// import { IGestion } from '@interfaces/gestion.interface';
 
 @Component({
 	selector: 'app-asta-regia',
-	imports: [InformacionesComponent],
+	imports: [InformacionesComponent, BotonesAddComponent],
 	templateUrl: './asta-regia.component.html',
 	styleUrl: './asta-regia.component.scss'
 })
 export default class AstaRegiaComponent {
-	private readonly router = inject(Router);
-	private _getNewsComsDocs = inject(GetNewsComsDocs);
-	public news: INew[] = [];
-	public coms: ICom[] = [];
-	public docs: IDoc[] = [];
-	public gestiones: IGestion[] = [];
+	// private readonly router = inject(Router);
+	// private _getNewsComsDocs = inject(GetNewsComsDocs);
+	// public news: INew[] = [];
+	// public coms: ICom[] = [];
+	// public docs: IDoc[] = [];
+	// public gestiones: IGestion[] = [];
 	public tag = 'astaRegia';
 
-	addCom(): void {
-		this.router.navigateByUrl('addCom/' + this.tag);
-	}
+	// addCom(): void {
+	// 	this.router.navigateByUrl('addCom/' + this.tag);
+	// }
 }
