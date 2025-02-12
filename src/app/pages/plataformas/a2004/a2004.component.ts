c32b1e5dfbf930f44ced3aecbcc67c22cc033a92
mamjerez <correomam@gmail.com>
2025-02-12 18:41:18 +0100

    refactor: enhance a2004 component structure and data fetching

--- src/app/pages/plataformas/a2004/a2004.component.ts
+++ src/app/pages/plataformas/a2004/a2004.component.ts
@@ -1,17 +1,43 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import InformacionesComponent from '@app/commons/components/informaciones/informaciones.component';
+import BotonesAddComponent from '@app/commons/components/botones-add/botones-add.component';
+import ComentariosComponent from '@app/commons/components/comentarios/comentarios.component';
+import DocumentosComponent from '@app/commons/components/documentos/documentos.component';
+import GestionesComponent from '@app/commons/components/gestiones/gestiones.component';
+import NoticiasComponent from '@app/commons/components/noticias/noticias.component';
+
+import { GetNewsComsDocs } from '@services/getNewsComsDocs.service';
+import { SupabaseService } from '@services/supabase.service';
+
+import { ICom } from '@interfaces/com.interface';
+import { IDoc } from '@interfaces/doc.interface';
+import { INew } from '@interfaces/new.interface';
+import { IGestion } from '@interfaces/gestion.interface';
 
 @Component({
 	selector: 'app-a2004',
-	imports: [InformacionesComponent],
+	imports: [ComentariosComponent, DocumentosComponent, GestionesComponent, NoticiasComponent, BotonesAddComponent],
 	templateUrl: './a2004.component.html',
 	styleUrl: './a2004.component.scss'
 })
-export default class A2004Component {
+export default class A2004Component implements OnInit {
 	private readonly router = inject(Router);
+	private readonly _supabaseService = inject(SupabaseService);
+	private _getNewsComsDocs = inject(GetNewsComsDocs);
 	public tag = 'a2004';
+	public news: INew[] = [];
+	public coms: ICom[] = [];
+	public docs: IDoc[] = [];
+	public gestiones: IGestion[] = [];
+
+	ngOnInit(): void {
+		this.fetchData();
+	}
+
+	async fetchData() {
+		[this.news, this.coms, this.docs, this.gestiones] = await this._getNewsComsDocs.fetchDataFromSupabase(this.tag);
+	}
 
 	addCom(): void {
 		this.router.navigateByUrl('addCom/' + this.tag);
