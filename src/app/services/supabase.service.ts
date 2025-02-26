701fdbaa4bc5404dd48b2e95b49c8e30023788ca
mamjerez <correomam@gmail.com>
2025-02-26 21:13:47 +0100

    feat: add cuota 2025 to ficha component and update data fetching in supabase service

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -187,7 +187,7 @@ export class SupabaseService {
 	async fetchDataAsociacionesById(id: string): Promise<any[]> {
 		const { data, error } = await this._supabase
 			.from('solidaridad_asociaciones')
-			.select('id,nombre,is_activa,rma,NIF,sede,distrito,barrio,email,solidaridad,cuota2023,cuota2024')
+			.select('id,nombre,is_activa,rma,NIF,sede,distrito,barrio,email,solidaridad,cuota2023,cuota2024,cuota2025')
 			.eq('id', id);
 
 		if (error) {
